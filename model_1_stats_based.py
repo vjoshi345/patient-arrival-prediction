f2b2a16b8c8275f72b581f170ba2db10e522b4e2
Varun Joshi <vjoshi345@gmail.com>
2023-02-25 18:27:41 -0500

    make the code reusable across data windows of diff size

--- model_1_stats_based.py
+++ model_1_stats_based.py
@@ -1,10 +1,12 @@
+import math
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 from utils import mae, mse, moving_average_forecast
 
 
-def run_model(inp_file, sev='ESI 3'):
+def run_model(inp_file, sev='ESI 3', split_time=30696, n_points=720, n_avg=168, n_seasonality=168,
+              win_size=11):
     print('\n*****************************************************')
     print('Running for input file: ', inp_file.split('/')[-1])
     print('Patient Severity: ', sev)
@@ -12,10 +14,6 @@ def run_model(inp_file, sev='ESI 3'):
     data = pd.read_csv(inp_file, dtype={sev: float})
     print(data.shape)
     print(data.dtypes)
-
-    # Split the dataset into train and validation sets
-    # (3.5 years for training and 0.5 years for validation)
-    split_time = 30696
 
     # Get the train and validation sets
     x_train = np.array(data[sev][:split_time])
@@ -35,7 +33,6 @@ def run_model(inp_file, sev='ESI 3'):
 
     # Model 2: moving average forecast
     print('\n *** Metrics for the moving average forecast ***')
-    n_points = 720  # last 30 days
     moving_avg = moving_average_forecast(np.array(data[sev]), n_points)[split_time - n_points:]
     print('MSE (tf):', tf.keras.metrics.mean_squared_error(x_valid, moving_avg).numpy())
     print('MSE (manual):', mse(x_valid, moving_avg))
@@ -44,8 +41,6 @@ def run_model(inp_file, sev='ESI 3'):
 
     # Model 3: moving average with differencing
     print('\n *** Metrics for moving average with differencing ***')
-    n_avg = 168  # last 7 days
-    n_seasonality = 168  # last 7 days
     series = np.array(data[sev])
 
     # Compute the series with differencing and then do a moving average
@@ -65,9 +60,10 @@ def run_model(inp_file, sev='ESI 3'):
 
     # Model 4: moving average with differencing (smoothed past)
     print('\n *** Metrics for moving average with differencing (smoothed past) ***')
+    left, right = math.floor(win_size/2.0), math.ceil(win_size/2.0)
     diff_moving_avg_plus_smooth_past = moving_average_forecast(
-        series[split_time - (n_seasonality + 5):-(n_seasonality - 6)],
-        11) + diff_moving_avg
+        series[split_time - (n_seasonality + left):-(n_seasonality - right)],
+        win_size) + diff_moving_avg
     print('MSE (tf):', tf.keras.metrics.mean_squared_error(x_valid, diff_moving_avg_plus_smooth_past).numpy())
     print('MSE (manual):', mse(x_valid, diff_moving_avg_plus_smooth_past))
     print('MAE (tf):', tf.keras.metrics.mean_absolute_error(x_valid, diff_moving_avg_plus_smooth_past).numpy())
@@ -75,4 +71,9 @@ def run_model(inp_file, sev='ESI 3'):
 
 
 if __name__ == '__main__':
-    run_model(inp_file='data/patient_arrival_data.csv', sev='ESI 3')
+    # Hourly forecasting
+    run_model(inp_file='data/patient_arrival_data.csv', sev='ESI 5')
+
+    # Weekly forecasting
+    run_model(inp_file='data/patient_arrival_weekly.csv', sev='ESI 3', split_time=182,
+              n_points=4, n_avg=4, n_seasonality=4, win_size=3)
